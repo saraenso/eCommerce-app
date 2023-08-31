32117d672a1c0422a0dbfc3aac2a43b5ada6187d
Sara Kairat <106602123+saraenso@users.noreply.github.com>
2023-08-31 10:26:02 +0300

    Delete src directory

--- src/js/preloader.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function() {
-    const loadingIndicator = document.getElementById('loadingIndicator');
-    if (loadingIndicator) {
-        loadingIndicator.style.display = 'none';
-    }
-});
